7433daff2af4d32e7c0547bc858e894287fcce42
Dandara <dands20melo@gmail.com>
2025-03-25 23:39:15 -0300

    Estrutura base

---
+++ app/(tabs)/index.tsx
@@ -0,0 +1,10 @@
+import { View, Text } from 'react-native';
+
+export default function OpeningApp() {
+  return (
+    <View style={{ height: '100%', display: 'flex', justifyContent:'center', width: '100%', alignItems: 'center' }}>
+      <Text style={{ fontFamily: 'Poppins', fontSize: '30px' }}>Cidade em Foco</Text>
+    </View>
+  );
+}
+
