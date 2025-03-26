c9b5a17d2f23c6f476e25526e92a356cf9120135
Dandara Melo <dands20melo@gmail.com>
2025-03-26 12:25:27 -0300

    :sparkles: feat: Criando tela inicial

---
+++ app/(tabs)/initialPageStyle.ts
@@ -0,0 +1,52 @@
+import { StyleSheet } from "react-native";
+
+export const InitialPageStyles = StyleSheet.create({
+    container: {
+        backgroundColor: "#6A0DAD",
+        height: '100%',
+        display: 'flex',
+        flex: 1,
+        width: '100%',
+        alignItems: 'center',
+        justifyContent: 'center',
+
+    },
+    img: {
+        width: 180,
+        height: 180,
+        marginBottom: 10,
+    },
+    title: {
+        fontSize: 30,
+        color: 'white',
+        fontFamily: 'PoppinsSemiBold',
+
+    },
+    caption: {
+        width: 250,
+        fontSize: 14,
+        textAlign: 'center',
+        color: 'white',
+        marginTop: 40,
+        marginBottom: 50,
+        fontFamily: 'PoppinsMedium',
+
+    },
+    button: {
+        width: 300,
+        height: 50,
+        backgroundColor: 'white',
+        alignItems: 'center',
+        justifyContent: 'center',
+        borderRadius: 10,
+        padding: 12,
+        marginBottom: 10,
+        elevation: 15,
+    },
+    
+    btnText: {
+        fontFamily: 'PoppinsMedium',
+        fontSize: 17,
+    }
+
+})
