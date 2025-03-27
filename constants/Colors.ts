acbeb2ee3309a0d648b21eefec4f46c778cc54ae
Dandara <dands0melo@gmail.com>
2025-03-27 10:55:50 -0300

    :sparkles: feat: Cores do projeto

--- constants/Colors.ts
+++ constants/Colors.ts
@@ -1,21 +1,8 @@
-const tintColorLight = '#0a7ea4';
-const tintColorDark = '#fff';
-
 export const Colors = {
-  light: {
-    text: '#11181C',
-    background: '#fff',
-    tint: tintColorLight,
-    icon: '#687076',
-    tabIconDefault: '#687076',
-    tabIconSelected: tintColorLight,
-  },
-  dark: {
-    text: '#ECEDEE',
-    background: '#151718',
-    tint: tintColorDark,
-    icon: '#9BA1A6',
-    tabIconDefault: '#9BA1A6',
-    tabIconSelected: tintColorDark,
-  },
+  primary: '#6A0DAD',
+  azul: '#1663C8',
+  amarelo: '#FFAC11',
+  verde: '#8FCF57',
+  vermelho: '#E72A2A',
+  background: '#E8E8E8',
 };
