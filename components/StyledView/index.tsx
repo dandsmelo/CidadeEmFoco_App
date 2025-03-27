8c2a1db4b0af00b553a0ea9a3624046de85afdd2
Dandara <dands0melo@gmail.com>
2025-03-27 10:56:49 -0300

    :sparkles: feat: Adicionando componente StyledView

---
+++ components/StyledView/index.tsx
@@ -0,0 +1,16 @@
+import { View, Text } from "react-native";
+import { style } from "./style";
+
+interface Props {
+    children: React.ReactNode
+}
+
+export default function StyledView(props: Props) {
+    const { children } = props;
+  return (
+    <View style={style.container}>
+      {children}
+    </View>
+  );
+}
+
---
+++ components/StyledView/style.ts
@@ -0,0 +1,12 @@
+import { Colors } from "@/constants/Colors";
+import { StyleSheet } from "react-native";
+
+export const style = StyleSheet.create({
+    container: {
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+        height: '100%',
+        backgroundColor: Colors.background,
+    }
+})
