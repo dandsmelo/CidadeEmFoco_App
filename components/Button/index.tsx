efac32670e6a7c6ab534793ae722858a9ff05f4c
Dandara <dands20melo@gmail.com>
2025-04-01 23:19:56 -0300

    :sparkles: feat: Criando componente StyledButton

---
+++ components/Button/index.tsx
@@ -0,0 +1,39 @@
+import { TouchableOpacity } from "react-native";
+import { Text } from "react-native";
+import { StyleSheet } from "react-native";
+import { Colors } from "@/constants/Colors";
+
+interface Props {
+    text: string;
+    background: keyof typeof Colors;
+}
+
+export default function StyledButton(props: Props) {
+    const { text, background } = props;
+
+    return (
+        <TouchableOpacity style={[styles.container, { backgroundColor: Colors[background] }]}>
+            <Text style={styles.text}>
+                {text}
+            </Text>
+        </TouchableOpacity>
+    )
+}
+
+const styles = StyleSheet.create({
+    container: {
+        display: 'flex',
+        height: 50,
+        width: 300,
+        padding: 10,
+        borderRadius: 10,
+        marginBottom: 10
+    },
+    text: {
+        fontSize: 20,
+        color: 'white',
+        fontFamily: 'PoppinsSemiBold',
+        width: '100%',
+        textAlign: 'center',
+    }
+})
