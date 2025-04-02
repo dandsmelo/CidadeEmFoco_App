c3f4984d09e9df7ef5b637590350e24af945deb4
Dandara <dands20melo@gmail.com>
2025-04-01 23:20:58 -0300

    :sparkles: feat: Tela de login finalizada

--- app/(tabs)/login/index.tsx
+++ app/(tabs)/login/index.tsx
@@ -1,17 +1,32 @@
-import { View, Text } from "react-native";
+import { View, Text, Image } from "react-native";
 import { style } from "./style";
 import StyledView from "@/components/StyledView";
 import StyledTitle from "@/components/StyledTitle";
+import StyledInputs from "@/components/StyledInputs";
+import StyledButton from "@/components/Button";
+import Icon from "react-native-vector-icons/FontAwesome5";
 
 export default function Login() {
+
   return (
     <StyledView>
       <View style={style.containerImg}>
-        <Text>jsdjhdjdscbs</Text>
+        <Icon name="chevron-left" size={25} style={style.icon} />
+        <Image source={require('@/assets/images/loginImg.png')} style={style.img} />
       </View>
       <View style={style.textView}>
         <StyledTitle title="Bem vindo de volta" />
         <Text>"Seja a voz da sua comunidade. Denuncie e inspire mudanças!"</Text>
+      </View>
+      <View>
+        <StyledInputs icon="user" placeholder="Username"/>
+        <StyledInputs icon="lock" placeholder="Senha" />
+      </View>
+      <View style={style.divBtn}>
+        <StyledButton text="Logar" background="amarelo"/>
+        <Text style={style.fgtPassword}>
+          Esqueci minha senha
+        </Text>
       </View>
     </StyledView>
   );
--- app/(tabs)/login/style.ts
+++ app/(tabs)/login/style.ts
@@ -5,25 +5,37 @@ export const style = StyleSheet.create({
     containerImg: {
         display: 'flex',
         justifyContent: 'center',
-        alignItems: 'center',
-        height: 250,
+        height: 210,
         width: '100%',
         backgroundColor: Colors.primary,
         top: 0,
-        position: 'absolute',
+        position: 'relative',
     },
-    title: {
-        color: Colors.primary,
+    icon: {
+        position: "absolute",
+        alignSelf: "flex-start",
+        color: "white",
+        top: 0,
+        margin: 15,
+    },
+    img: {
+        width: '100%',
+        height: 210,
+        marginTop: 18,
     },
     textView: {
-        display: 'flex',
         textAlign: 'left',
-        alignItems: 'flex-end',
-        flexDirection: 'column',
+        margin: 40,
     },
-    caption: {
-        width: 250,
-        margin: 20,
+    divBtn: {
+        marginTop: 20,
+        alignItems: 'center',
+    },
+    fgtPassword: {
+        color: 'black',
+        fontSize: 16,
+        fontFamily: 'PoppinsMedium',
+        textDecorationLine: "underline",
     }
 
 })
