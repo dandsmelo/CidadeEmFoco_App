19abf0e40c31889169c23dd55e01c8b2e5afee02
Dandara Melo <dands20melo@gmail.com>
2025-04-09 10:45:36 -0300

    :sparkles: feat: Tela minhas denúncias

---
+++ app/(tabs)/minhasDenuncias/index.tsx
@@ -0,0 +1,31 @@
+import NavBar from "@/components/NavBar";
+import { View, Text } from "react-native";
+import { style } from "./style";
+import { useCustomFonts } from "@/assets/fonts/Fonts";
+import Card from "@/components/Card";
+
+export default function MinhasDenuncias() {
+    const fontsLoaded = useCustomFonts()
+        
+          if (!fontsLoaded) {
+            return null; 
+          }
+
+    return (
+        <View style={style.container}>
+            <NavBar title="Minhas Denúncias"/>
+            <View style={style.cardView}>
+                <Card>
+                    <View style={style.topCard}>
+                        <Text style={style.title}>Título</Text>
+                        <Text style={style.data}>12/05/2025</Text>
+                    </View>
+                    <View style={style.endCard}>
+                        <Text style={style.category}>Buraco</Text>
+                        <Text style={style.status}>Pendente</Text>
+                    </View>
+                </Card>
+            </View>
+        </View>
+    )
+}
---
+++ app/(tabs)/minhasDenuncias/style.ts
@@ -0,0 +1,53 @@
+import { Colors } from "@/constants/Colors";
+import { StyleSheet } from "react-native";
+
+export const style = StyleSheet.create({
+    container: {
+        backgroundColor: Colors.background,
+        width: '100%',
+        height: '100%',
+    },
+    cardView: {
+        display: 'flex',
+        flexDirection: 'column',
+        justifyContent: 'center',
+        alignItems: 'center',
+        padding: 25,
+    },
+    topCard: {
+        display: 'flex',
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        width: '100%',
+        marginBottom: 5,
+    },
+    title: {
+        fontFamily: 'PoppinsBold',
+        fontSize: 20,
+    },
+    data: {
+        fontFamily: 'PoppinsRegular',
+    },
+    endCard: {
+        display: 'flex',
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        width: '100%',
+    },
+    category: {
+        padding: 8,
+        backgroundColor: Colors.primary,
+        borderRadius: 10,
+        color: 'white', 
+        fontFamily: 'PoppinsRegular',
+        marginTop: 5,   
+    },
+    status: {
+        padding: 8,
+        backgroundColor: Colors.vermelho,
+        borderRadius: 10,
+        color: 'white', 
+        fontFamily: 'PoppinsRegular',
+        marginTop: 5, 
+    }
+})
