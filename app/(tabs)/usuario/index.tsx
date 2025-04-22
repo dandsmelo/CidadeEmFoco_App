7433a89cbc88317335b529644e3fc9e8df8ce3fd
Dandara Melo <dands20melo@gmail.com>
2025-04-22 12:13:11 -0300

    :sparkles: feat: Adicionando modal de edição na página de usuário

--- app/(tabs)/usuario/index.tsx
+++ app/(tabs)/usuario/index.tsx
@@ -3,9 +3,19 @@ import StyledView from "@/components/StyledView";
 import { TouchableOpacity, View, Text } from "react-native";
 import { style } from "./style";
 import Icon from "react-native-vector-icons/FontAwesome5";
+import ModalEdicao from "./components/modalEditar";
+import { useState } from "react";
 
 export default function Usuario() {
     const fontsLoaded = useCustomFonts()
+
+    const [modalVisible, setModalVisible] = useState(false);
+    const [selectedField, setselectedField] = useState("");
+
+    const openModal = (campo: string) => {
+        setselectedField(campo);
+        setModalVisible(true);
+    };
             
     if (!fontsLoaded) {
         return null; 
@@ -21,15 +31,15 @@ export default function Usuario() {
                 <Text style={style.name}>Dandara Melo</Text>
             </View>
             <View style={style.inputsView}>
-                <TouchableOpacity style={style.btn}>
+                <TouchableOpacity style={style.btn} onPress={() => openModal("nome")}>
                     <Text style={style.text}>Nome</Text>
                     <Icon name="chevron-right" size={25} />
                 </TouchableOpacity>
-                <TouchableOpacity style={style.btn}>
+                <TouchableOpacity style={style.btn} onPress={() => openModal("telefone")}>
                     <Text style={style.text}>Telefone</Text>
                     <Icon name="chevron-right" size={25} />
                 </TouchableOpacity>
-                <TouchableOpacity style={style.btn}>
+                <TouchableOpacity style={style.btn} onPress={() => openModal("email")}>
                     <Text style={style.text}>Email</Text>
                     <Icon name="chevron-right" size={25} />
                 </TouchableOpacity>
@@ -41,8 +51,12 @@ export default function Usuario() {
                     <Text style={style.text}>Sair</Text>
                     <Icon name="chevron-right" size={25} />
                 </TouchableOpacity>
-
             </View>
+            <ModalEdicao
+                visible={modalVisible}
+                title={selectedField}
+                onClose={() => setModalVisible(false)}
+            />
         </StyledView>
     )
 }
