86dc6db3c776f923e228b62e64009f36dd464785
Mariana Julia <macedomarianajulia@gmail.com>
2025-10-16 17:47:54 -0300

    :lipstick:feat: Reestilização do cadastro

--- app/(tabs)/cadastro/index.tsx
+++ app/(tabs)/cadastro/index.tsx
@@ -1,244 +1,291 @@
-import {View, Text, TextInput, TouchableOpacity, Image, ScrollView} from "react-native";
+import {
+  View,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  Image,
+  ScrollView,
+} from "react-native";
 import { useCustomFonts } from "@/assets/fonts/Fonts";
 import { style } from "./style";
 import { useState } from "react";
-import Icon from 'react-native-vector-icons/FontAwesome5';
-import { router } from 'expo-router';
-import DropDownPicker from 'react-native-dropdown-picker';
+import Icon from "react-native-vector-icons/FontAwesome5";
+import IconI from "react-native-vector-icons/Entypo";
+import { router } from "expo-router";
+import DropDownPicker from "react-native-dropdown-picker";
 import { useFlashMessage } from "@/components/FlashMessageContext";
+import { LinearGradient } from "expo-linear-gradient";
 
+export default function Cadastro() {
+  const [open, setOpen] = useState(false);
+  const [tipo, setTipo] = useState(null);
+  const [items, setItems] = useState([
+    { label: "Cidadão", value: "cidadao" },
+    { label: "Servidor Público", value: "servidorPublico" },
+  ]);
 
-export default function Cadastro(){
-    const [open, setOpen] = useState(false);
-    const [tipo, setTipo] = useState(null);
-    const [items, setItems] = useState([
-        { label: 'Cidadão', value: 'cidadao' },
-        { label: 'Servidor Público', value: 'servidorPublico' }
-    ]);
+  const [isLoading, setIsLoading] = useState(false);
+  const fontsLoaded = useCustomFonts();
 
-    const [isLoading, setIsLoading] = useState(false);
-    const fontsLoaded = useCustomFonts()
-
-    if (!fontsLoaded) {
-      return null; 
-    }
-
-    const [nomeCompleto, setNomeCompleto] = useState("")
-    const [telefone, setTelefone] = useState("")
-    const [email, setEmail] = useState("")
-    const [senha, setSenha] = useState("")
-    const [confirmarSenha, setConfirmarSenha] = useState("")
-    const [senhaVisivel, setSenhaVisivel] = useState(false);
-    const [confirmarSenhaVisivel, setConfirmarSenhaVisivel] = useState(false);
-    const { showMessage } = useFlashMessage();
-
-
-    const handleCadastro = async () =>{
-        if (!nomeCompleto || !telefone || !email || !senha || !confirmarSenha || !tipo) {
-            showMessage("Por favor, preencha todos os campos.", 'warning')
-            return;
-        }
-
-        const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-        if (!emailRegex.test(email)) {
-            showMessage("Por favor, insira um e-mail válido.", 'warning');
-            return;
-        }
-
-        const telefoneRegex = /^\(?\d{2}\)?[\s-]?\d{4,5}-?\d{4}$/;
-        if (!telefoneRegex.test(telefone)) {
-            showMessage("Por favor, insira um número de telefone válido.", "warning");
-            return;
-        }
-
-        if (senha !== confirmarSenha) {
-            showMessage("As senhas não coincidem.", "warning");
-            return;
-        }
-
-         const senhaRegex = /^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{6,}$/;
-         if (!senhaRegex.test(senha)) {
-            showMessage("A senha deve ter pelo menos 6 caracteres e conter letras e números.", "warning");
-            return;
-        }
-
-        try {
-            const response = await fetch("http://localhost:3000/usuario", {
-                method: "POST",
-                headers: {
-                    "Content-Type": "application/json"
-                },
-                body: JSON.stringify({
-                    nome: nomeCompleto,
-                    telefone: telefone,
-                    email: email,
-                    senha: senha,
-                    tipo: tipo
-                })
-            });
-
-            const data = await response.json();
-
-            if (response.ok) {
-                setNomeCompleto("");
-                setTelefone("");
-                setEmail("");
-                setSenha("");
-                setConfirmarSenha("");
-                setTipo(null);
-                showMessage("Usuário cadastrado com sucesso!")
-                router.push('/login')
-    } else {
-        showMessage("Erro ao cadastrar. Tente novamente", 'error')
-    }
-
-    } catch (error) {
-    showMessage("Erro de conexão com o servidor. Verifique sua internet ou tente mais tarde.", 'error');
-    } finally {
-    setIsLoading(false);
+  if (!fontsLoaded) {
+    return null;
   }
-}
 
-    return(
-        <ScrollView>
-        <View style={style.container}>
+  const [nomeCompleto, setNomeCompleto] = useState("");
+  const [telefone, setTelefone] = useState("");
+  const [email, setEmail] = useState("");
+  const [senha, setSenha] = useState("");
+  const [confirmarSenha, setConfirmarSenha] = useState("");
+  const [senhaVisivel, setSenhaVisivel] = useState(false);
+  const [confirmarSenhaVisivel, setConfirmarSenhaVisivel] = useState(false);
+  const { showMessage } = useFlashMessage();
 
-            <View style={style.topoImg}>
-            <TouchableOpacity style={style.topoIcon}>
-                <Icon name="chevron-left" size={25} color="#FFFFFF" onPress={() => router.push('/')}/>
-            </TouchableOpacity>
-                <Image source={require('@/assets/images/cadastro.png')} style={style.img} />
-            </View>
+  const handleCadastro = async () => {
+    if (
+      !nomeCompleto ||
+      !telefone ||
+      !email ||
+      !senha ||
+      !confirmarSenha ||
+      !tipo
+    ) {
+      showMessage("Por favor, preencha todos os campos.", "warning");
+      return;
+    }
 
-            <View style={style.bodyText}>
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    if (!emailRegex.test(email)) {
+      showMessage("Por favor, insira um e-mail válido.", "warning");
+      return;
+    }
 
-            <View style={style.topoTexto}>
-            <Text style={style.title}>Registre-se</Text>
-            <Text style={style.text}>"A sua contribuição é valiosa. Ajude a tornar nossa cidade um lugar mais seguro!”</Text>
-            </View>
+    const telefoneRegex = /^\(?\d{2}\)?[\s-]?\d{4,5}-?\d{4}$/;
+    if (!telefoneRegex.test(telefone)) {
+      showMessage("Por favor, insira um número de telefone válido.", "warning");
+      return;
+    }
 
-            <View style={style.bodyInput}>
-            
-            <View>
-            <Icon name="user-alt" size={20} color="#898989" style={style.inputIcon} />
-            <TextInput
-            style={style.input}
-            placeholder="Nome Completo"
-            value={nomeCompleto}
-            onChangeText={setNomeCompleto}
-            placeholderTextColor="#898989"
+    if (senha !== confirmarSenha) {
+      showMessage("As senhas não coincidem.", "warning");
+      return;
+    }
+
+    const senhaRegex = /^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{6,}$/;
+    if (!senhaRegex.test(senha)) {
+      showMessage(
+        "A senha deve ter pelo menos 6 caracteres e conter letras e números.",
+        "warning"
+      );
+      return;
+    }
+
+    try {
+      const response = await fetch("http://localhost:3000/usuario", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          nome: nomeCompleto,
+          telefone: telefone,
+          email: email,
+          senha: senha,
+          tipo: tipo,
+        }),
+      });
+
+      const data = await response.json();
+
+      if (response.ok) {
+        setNomeCompleto("");
+        setTelefone("");
+        setEmail("");
+        setSenha("");
+        setConfirmarSenha("");
+        setTipo(null);
+        showMessage("Usuário cadastrado com sucesso!");
+        router.push("/login");
+      } else {
+        showMessage("Erro ao cadastrar. Tente novamente", "error");
+      }
+    } catch (error) {
+      showMessage(
+        "Erro de conexão com o servidor. Verifique sua internet ou tente mais tarde.",
+        "error"
+      );
+    } finally {
+      setIsLoading(false);
+    }
+  };
+
+  return (
+    <LinearGradient colors={["#6A0DAD", "#2C0547"]} style={style.container}>
+      <View style={style.topoImg}>
+        <TouchableOpacity style={style.topoIcon}>
+          <IconI
+            name="chevron-thin-left"
+            size={25}
+            color="#FFFFFF"
+            onPress={() => router.push("/")}
+          />
+        </TouchableOpacity>
+        <Image
+          source={require("@/assets/images/cadastro.png")}
+          style={style.img}
+        />
+      </View>
+
+      <View /*style={style.bodyText}*/>
+        <View style={style.topoTexto}>
+          <Text style={style.title}>Registre-se</Text>
+          <Text style={style.text}>
+            "A sua contribuição é valiosa. Ajude a tornar nossa cidade um lugar
+            mais seguro!”
+          </Text>
+        </View>
+
+        <View style={style.bodyInput}>
+          <View>
+            <Icon
+              name="user-alt"
+              size={20}
+              color="#898989"
+              style={style.inputIcon}
             />
-            </View>
-
-            <View>
-            <Icon name="mobile-alt" size={20} color="#898989" style={style.inputIcon} />
             <TextInput
-            style={style.input}
-            placeholder="Telefone"
-            keyboardType="phone-pad"
-            value={telefone}
-            onChangeText={setTelefone}
-            maxLength={11}
-            placeholderTextColor="#898989"
+              style={style.input}
+              placeholder="Nome Completo"
+              value={nomeCompleto}
+              onChangeText={setNomeCompleto}
+              placeholderTextColor="#898989"
             />
-            </View>
+          </View>
 
-            <View>
-            <Icon name="envelope" size={20} color="#898989" style={style.inputIcon} />
-            <TextInput
-            style={style.input}
-            placeholder="Email"
-            value={email}
-            onChangeText={setEmail}
-            placeholderTextColor="#898989"
+          <View>
+            <Icon
+              name="mobile-alt"
+              size={20}
+              color="#898989"
+              style={style.inputIcon}
             />
-            </View>
-
-            <View>
-            <Icon name="lock" size={20} color="#898989" style={style.inputIcon} />
             <TextInput
-                style={style.input}
-                placeholder="Senha"
-                value={senha}
-                onChangeText={setSenha}
-                secureTextEntry={!senhaVisivel}
-                placeholderTextColor="#898989"
+              style={style.input}
+              placeholder="Telefone"
+              keyboardType="phone-pad"
+              value={telefone}
+              onChangeText={setTelefone}
+              maxLength={11}
+              placeholderTextColor="#898989"
+            />
+          </View>
+
+          <View>
+            <Icon
+              name="envelope"
+              size={20}
+              color="#898989"
+              style={style.inputIcon}
+            />
+            <TextInput
+              style={style.input}
+              placeholder="Email"
+              value={email}
+              onChangeText={setEmail}
+              placeholderTextColor="#898989"
+            />
+          </View>
+
+          <View>
+            <Icon
+              name="lock"
+              size={20}
+              color="#898989"
+              style={style.inputIcon}
+            />
+            <TextInput
+              style={style.input}
+              placeholder="Senha"
+              value={senha}
+              onChangeText={setSenha}
+              secureTextEntry={!senhaVisivel}
+              placeholderTextColor="#898989"
             />
             <TouchableOpacity
-                style={style.inputIconRight}
-                onPress={() => setSenhaVisivel(!senhaVisivel)}
+              style={style.inputIconRight}
+              onPress={() => setSenhaVisivel(!senhaVisivel)}
             >
-                <Icon
+              <Icon
                 name={senhaVisivel ? "eye" : "eye-slash"}
                 size={20}
                 color="#898989"
-                />
+              />
             </TouchableOpacity>
-            </View>
+          </View>
 
-            <View>
-            <Icon name="lock" size={20} color="#898989" style={style.inputIcon} />
+          <View>
+            <Icon
+              name="lock"
+              size={20}
+              color="#898989"
+              style={style.inputIcon}
+            />
             <TextInput
-                style={style.input}
-                placeholder="Confirmar Senha"
-                value={confirmarSenha}
-                onChangeText={setConfirmarSenha}
-                secureTextEntry={!confirmarSenhaVisivel}
-                placeholderTextColor="#898989"
+              style={style.input}
+              placeholder="Confirmar Senha"
+              value={confirmarSenha}
+              onChangeText={setConfirmarSenha}
+              secureTextEntry={!confirmarSenhaVisivel}
+              placeholderTextColor="#898989"
             />
             <TouchableOpacity
-                style={style.inputIconRight}
-                onPress={() => setConfirmarSenhaVisivel(!confirmarSenhaVisivel)}
+              style={style.inputIconRight}
+              onPress={() => setConfirmarSenhaVisivel(!confirmarSenhaVisivel)}
             >
-                <Icon
+              <Icon
                 name={confirmarSenhaVisivel ? "eye" : "eye-slash"}
                 size={20}
                 color="#898989"
-                />
+              />
             </TouchableOpacity>
-            </View>
+          </View>
 
-            <View style={{ zIndex: 1000, marginHorizontal: 40, marginTop: 10 }}>
-                <DropDownPicker
-                open={open}
-                value={tipo}
-                items={items}
-                setOpen={setOpen}
-                setValue={setTipo}
-                setItems={setItems}
-                placeholder="Tipo de usuário"
-                style={{
-                    backgroundColor: '#FFFFFF',
-                    borderRadius: 10,
-                    borderColor: '#FFFFFF',
-                    height: 50,
-                }}
-                textStyle={{
-                    fontSize: 17,
-                    fontFamily: 'PoppinsMedium',
-                    color: '#000000',
-                }}
-                placeholderStyle={{
-                    color: '#898989',
-                }}
-                dropDownContainerStyle={{
-                    backgroundColor: '#FFFFFF',
-                    borderColor: '#FFFFFF',
-                }}
-                />
-                </View>
-
-            </View>
-
-            <View>
-            <TouchableOpacity style={style.button} onPress={handleCadastro}>
-                <Text style={style.textButton}>Cadastrar</Text>
-            </TouchableOpacity>
-            </View>
-
-            </View>
-            
+          <View style={{ zIndex: 1000, marginHorizontal: 40, marginTop: 10 }}>
+            <DropDownPicker
+              open={open}
+              value={tipo}
+              items={items}
+              setOpen={setOpen}
+              setValue={setTipo}
+              setItems={setItems}
+              placeholder="Tipo de usuário"
+              style={{
+                backgroundColor: "#FFFFFF",
+                borderRadius: 10,
+                borderColor: "#FFFFFF",
+                height: 50,
+              }}
+              textStyle={{
+                fontSize: 17,
+                fontFamily: "PoppinsMedium",
+                color: "#000000",
+              }}
+              placeholderStyle={{
+                color: "#898989",
+              }}
+              dropDownContainerStyle={{
+                backgroundColor: "#FFFFFF",
+                borderColor: "#FFFFFF",
+              }}
+            />
+          </View>
         </View>
-        </ScrollView>
-    )
+
+        <View>
+          <TouchableOpacity style={style.button} onPress={handleCadastro}>
+            <Text style={style.textButton}>Cadastrar</Text>
+          </TouchableOpacity>
+        </View>
+      </View>
+    </LinearGradient>
+  );
 }
--- app/(tabs)/cadastro/style.ts
+++ app/(tabs)/cadastro/style.ts
@@ -1,121 +1,117 @@
 import { StyleSheet } from "react-native";
 
 export const style = StyleSheet.create({
-    container: {
-        height: '100%',
-        display: 'flex',
-        flex: 1,
-        width: '100%',
-        alignItems: 'center',
+  container: {
+    height: "100%",
+    display: "flex",
+    flex: 1,
+    width: "100%",
+    alignItems: "center",
+  },
 
-    },
+  //TOPO
 
-    //TOPO
+  topoImg: {
+    width: "100%",
+    alignItems: "center",
+    height: 256,
+    position: "relative",
+  },
 
-    topoImg:{
-        backgroundColor: "#6A0DAD",
-        width: '100%',
-        alignItems: 'center',
-        height: 256,
-        position: 'relative',
+  img: {
+    marginTop: 80,
+    width: 320,
+    height: 180,
+  },
 
-    },
+  topoIcon: {
+    position: "absolute",
+    alignSelf: "flex-start",
+    color: "white",
+    top: 25,
+    margin: 15,
+  },
 
-    img: {
-        marginTop: 50,
-        width: 370,
-        height: 220, 
-    },
+  //TEXTO
 
-    topoIcon:{
-        position: "absolute",
-        alignSelf: "flex-start",
-        color: "white",
-        top: 0,
-        margin: 15,
-    },
+  /*bodyText: {
+    backgroundColor: "#E8E8E8",
+    width: "100%",
+  },*/
 
-    //TEXTO
+  topoTexto: {
+    marginLeft: 40,
+    marginRight: 40,
+  },
 
-    bodyText:{
-        backgroundColor:"#E8E8E8",
-        width: '100%',
-        
-    },
+  title: {
+    fontSize: 30,
+    color: "#FFFFFF",
+    fontFamily: "PoppinsSemiBold",
+    marginBottom: 5,
+    marginTop: 10,
+    textAlign: "center",
+  },
 
-    topoTexto:{
-        marginLeft:40,
-        marginRight:40,
-    },
+  text: {
+    fontSize: 13,
+    fontFamily: "PoppinsRegular",
+    marginBottom: 40,
+    color: "#FFFFFF",
+    textAlign: "center",
+  },
 
-    title:{
-        fontSize: 30,
-        color: "#6A0DAD",
-        fontFamily: 'PoppinsSemiBold',
-        marginBottom: 5,
-        marginTop:50,
-    },
+  //INPUT
 
-    text:{
-        fontSize: 15,
-        fontFamily: 'PoppinsRegular',
-        marginBottom: 10,
-    },
+  bodyInput: {
+    position: "relative",
+  },
 
-    //INPUT
+  input: {
+    backgroundColor: "#FFFFFF",
+    fontSize: 17,
+    fontFamily: "PoppinsMedium",
+    marginLeft: 40,
+    marginRight: 40,
+    marginTop: 10,
+    borderColor: "#FFFFFF",
+    borderWidth: 1,
+    borderRadius: 10,
+    padding: 10,
+    color: "#000000",
+    paddingLeft: 42,
+  },
 
-    bodyInput:{
-        position: 'relative',
-    },
-    
-    input:{
-        backgroundColor: "#FFFFFF",
-        fontSize: 17,
-        fontFamily: 'PoppinsMedium',
-        marginLeft:40,
-        marginRight:40,
-        marginTop: 10,
-        borderColor: "#FFFFFF",
-        borderWidth: 1,
-        borderRadius: 10,
-        padding: 10,
-        color: "#000000",
-        paddingLeft: 42,
-    },
+  inputIcon: {
+    top: 20,
+    left: 55,
+    position: "absolute",
+    zIndex: 1,
+  },
 
-    
-    inputIcon:{
-        top: 20,
-        left: 55,
-        position: 'absolute',
-        zIndex: 1,
+  inputIconRight: {
+    position: "absolute",
+    right: 55,
+    top: 20,
+  },
 
-    },
+  //BOTÃO
 
-    inputIconRight: {
-        position: 'absolute',
-        right: 55,
-        top: 20,
-    },
-
-    //BOTÃO
-
-    button:{
-        backgroundColor: "#1663C8",
-        marginLeft:40,
-        marginRight:40,
-        marginTop: 30,
-        marginBottom: 20, 
-        padding: 10,
-        borderColor: "#1663C8",
-        borderWidth: 1,
-        borderRadius: 10,
-        alignItems: 'center',
-    },
-    textButton:{
-        color: "#FFFFFF",
-        fontSize: 20,
-        fontFamily: 'PoppinsMedium',
-    },
-
-})
+  button: {
+    backgroundColor: "#1663C8",
+    marginLeft: 40,
+    marginRight: 40,
+    marginTop: 30,
+    marginBottom: 20,
+    padding: 10,
+    borderColor: "#1663C8",
+    borderWidth: 1,
+    borderRadius: 10,
+    alignItems: "center",
+  },
+  textButton: {
+    color: "#FFFFFF",
+    fontSize: 20,
+    fontFamily: "PoppinsMedium",
+  },
+});
